c97326a1704436f9c0d1ef40d4859ce09ada11ac
crazypeace <txican@gmail.com>
2026-01-11 20:13:49 +0800

    Create test-install.sh

---
+++ test-install.sh
@@ -0,0 +1,11 @@
+echo -e "                     _ ___                   \n ___ ___ __ __ ___ _| |  _|___ __ __   _ ___ \n|-_ |_  |  |  |-_ | _ |   |- _|  |  |_| |_  |\n|___|___|  _  |___|___|_|_|___|  _  |___|___|\n        |_____|               |_____|        "
+echo
+echo "推荐使用 三色图中央 的 VPS (不是"
+echo "https://my.racknerd.com/aff.php?aff=1374&pid=924"
+echo
+echo "这个才是 三色图中央 的 VPN (才怪"
+echo "https://justmysocks.net/members/aff.php?aff=256&gid=1"
+echo
+echo "这个链接是 sing-box 的压缩包"
+echo "https://github.com/SagerNet/sing-box/releases/download/v1.12.15/sing-box-1.12.15-darwin-amd64.tar.gz"
+echo
