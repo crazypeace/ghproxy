8f8a76eb4db739f5195a5bfcbcbfae42487c1b0d
crazypeace <txican@gmail.com>
2026-01-11 22:43:02 +0800

    fake

--- fake/test-install.sh
+++ fake/test-install.sh
@@ -1,21 +1,35 @@
+RESET='\e[0m'
+
+RED='\e[31m'             # 稳定
+ORANGE='\e[38;5;208m'    # 贵（256色）
+PURPLE='\e[35m'          # 慢 / 吐槽
+YELLOW='\e[33m'          # 速度
+GREEN='\e[32m'           # 差 / 链接
+BLUE='\e[34m'            # 便宜
+
 echo -e "         ____"
 echo -e "        /    \\"
-echo -e "       / \e[31m稳定\e[0m \\"
+echo -e "       / ${RED}稳定${RESET} \\"
 echo -e "      /        \\"
 echo -e "     +----------+"
-echo -e "    /\\ \e[93m贵\e[0m /\\ \e[35m慢\e[0m /\\"
+echo -e "    /\\ ${ORANGE}贵${RESET} /\\ ${PURPLE}慢${RESET} /\\"
 echo -e "   /  \\  /  \\  /  \\"
 echo -e "  /    \\/____\\/    \\"
-echo -e "  \\ \e[33m速度\e[0m\\ \e[32m差\e[0m / \e[34m便宜\e[0m/"
+echo -e "  \\ ${YELLOW}速度${RESET}\\ ${GREEN}差${RESET} / ${BLUE}便宜${RESET}/"
 echo -e "   \\     \\  /     /"
 echo -e "    \\_____\\/_____/"
 echo
-echo "推荐使用 三色图中央 的 VPS (不是"
-echo "https://my.racknerd.com/aff.php?aff=1374&pid=924"
+
+
+echo -e "推荐使用 三色图中央 的 VPS ${PURPLE}(不是${RESET}"
+echo -e "${GREEN} https://my.racknerd.com/aff.php?aff=1374&pid=924 ${RESET}"
 echo
-echo "这个才是 三色图中央 的 VPN (才怪"
-echo "https://justmysocks.net/members/aff.php?aff=256&gid=1"
+
+echo -e "这个才是 三色图中央 的 VPN ${PURPLE}(才怪${RESET}"
+echo -e "${GREEN} https://justmysocks.net/members/aff.php?aff=256&gid=1 ${RESET}"
 echo
-echo "现在下载sing-box 的压缩包"
-echo "https://github.com/SagerNet/sing-box/releases/download/v1.12.15/sing-box-1.12.15-windows-amd64.zip"
-wget "https://github.com/SagerNet/sing-box/releases/download/v1.12.15/sing-box-1.12.15-windows-amd64.zip"
+
+echo -e "现在下载 sing-box 的压缩包 ${PURPLE}(吗?${RESET}"
+echo -e "${GREEN} https://github.com/SagerNet/sing-box/releases/download/v1.12.15/sing-box-1.12.15-windows-amd64.zip ${RESET}"
+
+wget -nv "https://github.com/SagerNet/sing-box/releases/download/v1.12.15/sing-box-1.12.15-windows-amd64.zip"
